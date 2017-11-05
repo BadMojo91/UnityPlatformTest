ec879b4aa814de7d36c3741992af8f86e840301c
BadMojo91 <badmojo91@gmail.com>
2017-11-05 19:49:34 +1030

    Added parallax

---
+++ Assets/Scripts/World/ParallaxBackdrop.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxBackdrop : MonoBehaviour {
+    public float scale;
+    public float soften = 1f;
+    public float layer = 1f;
+    private Transform camPos;
+    private Vector3 prevCamPos;
+
+    void Awake() {
+        camPos = Camera.main.transform;
+    }
+
+    void Start() {
+        prevCamPos = camPos.position;
+    }
+
+    void Update() {  
+        float parallax = (prevCamPos.x - camPos.position.x) * scale;
+        float backgroundTargetPosX = transform.position.x + parallax;
+        Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, transform.position.y, layer);
+        transform.position = Vector3.Lerp(transform.position, backgroundTargetPos, soften * Time.deltaTime);
+        prevCamPos = camPos.position;
+    }
+}
